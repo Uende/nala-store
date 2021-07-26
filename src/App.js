571e556b4436e7db3f6d2f311df34ca5804cb046
Uende Teles <66647730+Uende@users.noreply.github.com>
2021-07-25 23:54:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Routes from './routes'
 import './GlobalStyle.css';
 import Footer from './pages/Home/components/Footer'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
+      <Home />
       <Routes />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
